6e454b294eaec1646a8861a152a5be5401f08d47
정인진_콘텐츠서비스개발 <injin.jeong@oliveyoung.co.kr>
2024-02-10 09:27:10 +0900

    first commit

---
+++ src/main/kotlin/com/me/injin/testcodewitharchitecturekotlin/TestCodeWithArchitectureKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.me.injin.testcodewitharchitecturekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestCodeWithArchitectureKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestCodeWithArchitectureKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/me/injin/testcodewitharchitecturekotlin/TestCodeWithArchitectureKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.me.injin.testcodewitharchitecturekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestCodeWithArchitectureKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
